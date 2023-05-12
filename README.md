ea14d3f45a9047593ad7e8592abb44ef01f8b14d
IKarinI <97239784+IKarinI@users.noreply.github.com>
2023-05-12 20:07:36 +0300

    LK 3 kuvamine, retsept by id
    ILMA NAV BAR'ITA

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # MidaToitu2.0
+
+Tegemist on enda varasemalt loodud projekti edasiarendusega õppimise eesmärkidel. 
+MidaToitu:
+[Veebileht](https://ikarini.github.io/MidaToitu/ )
+[Projekt](https://github.com/IKarinI/MidaToitu)
+
+### Projektiplaan 1
+Panen paika esmase projektiplaani, et saaks hakata samm-sammult funktsionaalsusi ühest projektist üle tooma teise peale.  
+Kõigepealt tegelen projekti LK3 ja LK2
+
+- Teha leht, mis võtab retsepide failist ühe konkreetset retsepti id alusel / getRecipeById. Tegelikult võiks retsepte kuvada nime järgi, et siis aadressiribale jääks pigem nimi, aga esialgu lihtsus mõttes teen kuvamise id kaudu.
+- Tekitan aadressi /recipes/:id, mis kuvab ühe retsepti, kasutab retsepti saamiseks getRecipeById
+- Tekitan aadress /recipes, mis kuvab kõik retseptid, mis failis on. Loon funktsiooni getAllRecipes
+- Tekitan aadress /recipes/tulemused, mis kuvab vastavalt kasutaja valitud toiduainetele (toiduainete id'dega array), kõik sobivad retseptid. Kasutan juba tehtud getRecipesById ja kõigi sobivate kuvamiseks käin toiduainete massiivi läbi.
+
+NOTES FORE LATER: 
+- getRecipeById võiks olla mitte id vaid retseptinime baasil. 
+- Nav bar lisamine
--- app.js
+++ app.js
@@ -1,12 +1,81 @@
 const express = require('express');
-const recipes = require('./recipes');
+const path = require('path');
+
+const app = express(); // Calling express as a function
+
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+const allRecipes = require('./recipeslist');
+
 const ingredients = require('./ingredients');
 
-const app = express();
+const recipes = require('./recipes');
+
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+recipes.initStudents(allRecipes);
+
+// eslint-disable-next-line no-undef
+
+
+const header = (heading) => `
+<!DOCTYPE html>
+<html lang="et">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="/style.css" />
+    <title>${heading}</title>
+  </head>
+
+  <body>
+    <div class="vjoon" id="vj1"></div>
+    <div class="vjoon" id="vj2"></div>
+    <div class="vjoon" id="vj3"></div>
+    <div class="v2line">`;
+
+const footer = `
+  </div>
+  </body>
+  </html>`;
+
+app.get('/retseptid/:id', (req, res) => {
+  const number = req.params.id;
+  const recipe = recipes.getRecipeById(number);
+  if (recipe === null) {
+    return res.send('Sellist retsepti ei leidnud');
+  }
+  let response = `
+    ${header(recipe.retseptiNimi)}
+    <div class="vkast3">
+    <div class="skast3">
+      <div class="nimekast">
+        <div class="nimi">${recipe.retseptiNimi}</div>
+      </div>
+      <div class="ylemine">
+        <div class="peaminepilt">
+          <img src="/${recipe.pilt}" class="pilt"></div>
+        <div class="koostisosadValmistamine">
+          <div class="koostisosadPea">
+            <h3 class="pealkiri2">Koostisosad:</h3>`;
+  for (let i = 0; i < recipe.koostisosadeNimekiri.length - 1; i++) {
+    response += `
+            <div class="koostisosad">${recipe.koostisosadeNimekiri[i]}</div>`
+  }
+  response += `
+          </div>
+          <div class="valmistamiseKast">
+            <h3 class="pealkiri2">Valmistamine:</h3>
+            <div class="valmistamine">${recipe.valmistamine}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+   ${footer}`;
+  return res.send(response);
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
