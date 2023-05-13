74b82fb48c90b72595845057c9360b6f90ddcbcf
IKarinI <97239784+IKarinI@users.noreply.github.com>
2023-05-13 10:13:12 +0300

    recipes.js parandused

--- recipes.js
+++ recipes.js
@@ -22,20 +22,28 @@ function getAllRecipes() {
 function getSuitableRecipes(results) {
   let missingItems = [];
   let recipesToShow = [];
-  const obj = {};
-  if (results === []) {
+  let obj = {};
+  // if (results === []) { See võrdlus ei pruugi töötada
+  if (results.length < 1) {
     recipesToShow = getAllRecipes();
   } else {
     for (let i = 0; i < recipes.length; i++) { // Iga retsept
+      missingItems = [];
+      obj = {};
       for (let j = 0; j < recipes[i].koostisosad.length; j++) {
       // Iga element retsepti koostisosade listis
-        if (results.includes(recipes[i].koostisosad[j]) === false) {
+        // if (results.includes(recipes[i].koostisosad[j]) === false) {
+        // Lihtsam viis, kuidas kontrollida
+        if (!results.includes(recipes[i].koostisosad[j])) {
           missingItems.push(recipes[i].koostisosad[j]);
         }
       }
       if (missingItems.length <= 3) {
-        obj.recipeId = recipes.id;
-        obj.noItemsList = missingItems;
+        obj.recipeId = recipes[i].id;
+        // obj.noItemsList = [missingItems];
+        // eslint-disable-next-line max-len
+        // sellega omistad viite sellele originaalmassiivile ja kui seda muudad siis muutub ka objekti sees
+        obj.noItemsList = [missingItems];
         recipesToShow.push(obj);
       }
     }
